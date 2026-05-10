30ff33142e965bb54bf58600b665b174f6cdf68a
Galip Efe Öncü <galipefe75@gmail.com>
2026-05-11 00:04:14 +0300

    chore: Migrate to new design system stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { LanguageProvider } from './context/LanguageContext.jsx';
+import './styles/design-system.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
