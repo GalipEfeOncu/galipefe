b2abd3758f95eed4d41f45bc05ca26292febaf2f
Galip Efe Öncü <galipefe75@gmail.com>
2026-06-09 16:16:07 +0300

    prepare vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { LanguageProvider } from './context/LanguageContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './styles/design-system.css';
 import './index.css';
 
@@ -11,6 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HashRouter>
       <LanguageProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </LanguageProvider>
     </HashRouter>
   </React.StrictMode>
